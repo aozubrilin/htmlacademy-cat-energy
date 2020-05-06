1a79fd9f70629a6beaf89e7719503f1f78d8fbf8
aozubrilin <alex14_09@mail.ru>
2020-05-06 19:17:22 +0500

    Правки по БЭМ

--- source/js/exampl.js
+++ source/js/exampl.js
@@ -2,16 +2,16 @@ var examplBefore = document.querySelector(".example__image--before");
 var examplBeforeBtn = document.querySelector(".example__toggle--before");
 var examplAfter = document.querySelector(".example__image--after");
 var examplAfterBtn = document.querySelector(".example__toggle--after");
-var exempleRangToggl = document.querySelector(".exemple__range-toggle");
+var exempleRangToggl = document.querySelector(".example__range-toggle");
 
 examplAfterBtn.addEventListener("click", function () {
   examplBefore.style.display = "none";
   examplAfter.style.display = "block";
-  exempleRangToggl.classList.add("exemple__range-toggle--after");
+  exempleRangToggl.classList.add("example__range-toggle--after");
 });
 
 examplBeforeBtn.addEventListener("click", function () {
   examplBefore.style.display = "block";
   examplAfter.style.display = "none";
-  exempleRangToggl.classList.remove("exemple__range-toggle--after");
+  exempleRangToggl.classList.remove("example__range-toggle--after");
 });
